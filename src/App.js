a8318aeb2d61601e8f518bc411f871415bd468cf
Vaidas <vaidas.makstutis@gmail.com>
2022-12-02 20:49:25 +0200

    Stopwatch timer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const App = () => {
+  const [time, setTime] = React.useState(0);
+  const [timerOn, setTimerOn] = React.useState(false);
+
+  React.useEffect(() => {
+    let interval = null;
+    if (timerOn) {
+      interval = setInterval(() => {
+        setTime(prevTime => prevTime + 10);
+      }, 10);
+    } else {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [timerOn]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="timers-container d-flex flex-column align-items-center mt-5">
+        <h1>Stopwatch</h1>
+        <div id="display" className="fs-3 mt-2">
+          <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+          <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
+          <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
+        </div>
+        <div id="buttons" className="mt-4">
+          {!timerOn && time === 0 && (
+            <button className="btn btn-primary mx-2" onClick={() => setTimerOn(true)}>
+              Start
+            </button>
+          )}
+          {timerOn && (
+            <button className="btn btn-secondary mx-2" onClick={() => setTimerOn(false)}>
+              Stop
+            </button>
+          )}
+          {!timerOn && time !== 0 && (
+            <button className="btn btn-primary mx-2" onClick={() => setTimerOn(true)}>
+              Resume
+            </button>
+          )}
+          {!timerOn && time > 0 && (
+            <button className="btn btn-danger mx-2" onClick={() => setTime(0)}>
+              Reset
+            </button>
+          )}
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
